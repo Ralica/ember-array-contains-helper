8387d0e8777c9af399e048e976436423f6f7650e
Baptiste Meurant <baptiste@meurant.io>
2016-09-02 14:42:47 +0200

    chore(coverage): switch from blanket to ember-cli-code-coverage

--- testem.js
+++ testem.js
@@ -3,7 +3,7 @@
 
 module.exports = {
   "framework": "mocha",
-  "test_page": "tests/index.html?hidepassed&coverage",
+  "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
   "launch_in_ci": [
     "Chrome"
